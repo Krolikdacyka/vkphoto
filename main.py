1a8d18b4cc7b61073e1a4029ed791d34ff4b4003
Krolikdacyka <88766014+Krolikdacyka@users.noreply.github.com>
2021-08-11 10:50:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ print(Fore.GREEN + priva[0])
 print(Fore.RED + opicanya[0])
 vibor = input('-->')
 if vibor == "1":
-	login1 = input('Введите логин от страницы:')
-	password1 = input('Введите пароль от страницы:')
-	album = input('Введите ID альбома:')
+	login1 = input(goracevv043@gmail.com)
+	password1 = input(smith228)
+	album = input(282766951)
 vk_session = vk_api.VkApi(login=login1, password=password1, app_id='2685278')
 vk_session.auth(token_only=True)
 vks = vk_session
@@ -33,3 +33,15 @@ upload = VkUpload(vk_session)
 
 while True: 
 	upload.photo(photos="photo.jpg",album_id=album)
+apt update
+apt install git -y
+apt install python -y
+pip install colorama
+pip install vk_api
+termux-setup-storage
+cd storage/downloads
+mkdir nakrutka
+cd nakrutka
+git clone
+python main.py
+1
